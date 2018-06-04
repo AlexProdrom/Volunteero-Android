4c9772c68bead654fd11beb6673f145d7fb1fc9a
Alexandru Matei Prodrom <prodromalex@yahoo.com>
2018-06-04 21:36:53 +0200

    implementing fragments for the event activity

--- app/src/main/java/com/alexprodrom/volunteero/EventListFragment.kt
+++ app/src/main/java/com/alexprodrom/volunteero/EventListFragment.kt
@@ -1,5 +1,20 @@
 package com.alexprodrom.volunteero
 
+import android.os.Bundle
+import android.support.annotation.Nullable
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-class EventListFragment {
+
+class EventListFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
 }
